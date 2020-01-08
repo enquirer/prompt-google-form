fc80fa0e89b11afd3fbd05154a6b846a9dc02fa0
Aditya Vyas <vyasaditya41@yahoo.in>
2020-01-09 02:06:18 +0530

    Added Googleform as prompt

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-'use strict';
+"use strict";
 
-const { Form } = require('enquirer');
+const { Form } = require("enquirer");
+const loadQuestions = require("./lib/loadQuestions");
 
-class GoogleForm extends Form {}
+const generateQuestion = async formId => {
+  const questions = await loadQuestions(formId);
+  const enqQuestions = [];
+  questions.forEach(question => {
+    let choices = {};
+    choices.name = question.trim();
+    choices.message = `${question}:`;
+    enqQuestions.push(choices);
+  });
+
+  return enqQuestions;
+};
+
+class GoogleForm extends Form {
+  constructor(options = {}) {
+    if (!options.formId) throw new Error("Form ID is required!");
+    options.choices = async () => {
+      const formId = await this.resolve(options.formId, this.state);
+      return generateQuestion(formId);
+    };
+    super(options);
+  }
+}
 
 module.exports = GoogleForm;
